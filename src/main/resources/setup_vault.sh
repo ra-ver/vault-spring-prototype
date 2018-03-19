f358943020aaf2bf709e1b71f7d05f311bccdbef
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-03-19 11:43:27 -0400

    Read all config values from Vault

--- src/main/resources/setup_vault.sh
+++ src/main/resources/setup_vault.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # start Vault server using:
 ./vault server -config vault.conf
 
@@ -12,4 +13,4 @@
 # write data: refer this for more examples: https://github.com/mp911de/spring-cloud-vault-config-samples/blob/master/src/test/bash/setup_examples.sh
 ./vault write secret/vault-proto mykey=myvalue hello.world='Hello, World'
 
-./vault write secret/vault-proto database.username=myuser database.password=mypassword
+./vault write secret/vault-proto/development mykey=development hello.world='Hello Developer' dog.name='Vault Puppy' dog.age=3 cat.name='Vault Meow' cat.age=4
